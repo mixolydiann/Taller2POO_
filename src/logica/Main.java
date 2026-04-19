958876f4048ae188708d5770e331c6271a6f6be0
Luis Molina <mixolydiannn@gmail.com>
2026-04-18 20:00:05 -0400

    initial commit

---
+++ src/logica/Main.java
@@ -0,0 +1,18 @@
+package logica;
+/*
+Luis Molina / 21.564.225-9 / mixolydiann
+Vicente Guerra / 21.855.415-6 / nemura0
+*/
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> names = new ArrayList<String>();
+		
+		System.out.println(names.size());
+	}
+
+}
