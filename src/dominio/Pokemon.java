e450fb154bca0445ebf3a2283b077384442be8ef
Luis Molina <mixolydiannn@gmail.com>
2026-05-04 21:56:53 -0400

    primer commit (Clases y main wip)

---
+++ src/dominio/Pokemon.java
@@ -0,0 +1,42 @@
+package dominio;
+
+public class Pokemon {
+    
+    // Atributos base (De Pokedex.txt)
+    private String nombre;
+    private String habitat;
+    private double porcentajeAparicion;
+    private int vida;
+    private int ataque;
+    private int defensa;
+    private int ataqueEspecial;
+    private int defensaEspecial;
+    private int velocidad;
+    private String tipo;
+    
+    // Atributo de estado (De Registros.txt)
+    private String estado; // Puede ser "Vivo" o "Debilitado"
+    
+    // Constructor
+    public Pokemon(String nombre, String habitat, double porcentajeAparicion, int vida, int ataque, 
+                   int defensa, int ataqueEspecial, int defensaEspecial, int velocidad, String tipo) {
+        this.nombre = nombre;
+        this.habitat = habitat;
+        this.porcentajeAparicion = porcentajeAparicion;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.ataqueEspecial = ataqueEspecial;
+        this.defensaEspecial = defensaEspecial;
+        this.velocidad = velocidad;
+        this.tipo = tipo;
+        this.estado = "Vivo"; // Por defecto, un Pokémon capturado o leído nuevo está Vivo
+    }
+
+    // Método utilitario para los combates
+    public int obtenerSumaStats() {
+        return vida + ataque + defensa + ataqueEspecial + defensaEspecial + velocidad;
+    }
+
+    // (Aquí irían todos los Getters y Setters)
+}
--- src/logica/Main.java
+++ src/logica/Main.java
@@ -4,15 +4,54 @@ Luis Molina / 21.564.225-9 / mixolydiann
 Vicente Guerra / 21.855.415-6 / nemura0
 */
 
-import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		ArrayList<String> names = new ArrayList<String>();
+		boolean ingame = true;
 		
-		System.out.println(names.size());
+		Scanner sc = new Scanner(System.in);
+		
+		while(ingame) {
+			
+			System.out.println("1) Continuar.");
+			System.out.println("2) Nueva partida.");
+			System.out.println("3) Salir");
+			System.out.println();
+			System.out.print("> Ingrese opcion: "); 
+			
+			try {
+				
+				int mainm = sc.nextInt();
+				sc.nextLine(); 
+				
+				switch(mainm) {
+				case 1:
+					System.out.println("Elegiste Continuar.");
+					break;
+				case 2:
+					System.out.println("Elegiste Nueva Partida.");
+					break;
+				case 3:
+					ingame = false;
+					System.out.println("Ha cerrado el programa.");
+					break;
+				default:
+					System.out.println("Input no reconocido. ingresa 1, 2 o 3.");
+				}
+				
+			} catch (InputMismatchException e) {
+				System.out.println("¡Error! Debes ingresar un caracter numerico.");
+				sc.nextLine(); 
+			}
+			System.out.println(); 
+		}
+		
+		
+		sc.close();
 	}
 
 }
